65cfc8dc703d9ca3e7e4092a2f69f6b4b95c19c1
DAntonenko <antonenko.dv@esphere.ru>
2021-08-14 03:00:51 +0300

    enter and registration forms started

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -5,10 +5,14 @@ import logo from '../../assets/logo.svg';
 import './StartPage.scss';
 
 export class StartPage extends React.Component {
+  state = {
+    currentMode: 'enter',
+  }
+
   render() {
     return (
       <div className='start-page'>
-       <div className='start-page__sidebar'>
+       <section className='start-page__sidebar'>
          <div className='start-page__identity'>
           <img
             className='start-page__emblem'
@@ -21,7 +25,53 @@ export class StartPage extends React.Component {
             alt='логотип Лофт-Такси'
           />
          </div>
-       </div>
+       </section>
+       <section className='start-page__form-field'>
+         {this.state.currentMode === 'enter' && 
+            <form className='start-page__form start-page__form--enter'>
+              <h2 className='start-page__form-title'>Войти</h2>
+              <label className='start-page__label'>
+                Email
+                <input className='start-page__input' type='email' placeholder='mail@mail.ru' />
+              </label>
+              <label className='start-page__label'>
+                Пароль
+                <input className='start-page__input' type='password' placeholder='*************' />
+              </label>
+              <button
+                className = 'start-page__forgot-password-button'
+                type='button'
+              >
+                Забыли пароль?
+              </button>
+              <p className='start-page__mode-change'>
+                Новый пользователь?
+                <button
+                  className = 'start-page__mode-change-button'
+                  type='button'
+                  onClick={() => this.setState({currentMode: 'registration'})}
+                >
+                  Регистрация
+                </button>
+              </p>
+            </form>
+         }
+         {this.state.currentMode === 'registration' &&
+            <form className='start-page__form start-page__form--registration'>
+              <h2 className='start-page__form-title'>Регистрация</h2>
+              <p className='start-page__mode-change'>
+                Уже зарегистрированны?
+                <button
+                  className = 'start-page__mode-change-button'
+                  type='button'
+                  onClick={() => this.setState({currentMode: 'enter'})}
+                >
+                  Войти
+                </button>
+              </p>
+            </form>
+         }
+       </section>
       </div>
     )
   }
